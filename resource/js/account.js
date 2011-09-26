9c479a24c9de7ee394f554d3bc47a54ff36a0296
David Lin <xkdavid@gmail.com>
2011-09-26 17:14:55 +0800

    Add simpledialog JS and CSS files
    Include the simpledialog JS and CSS file in index.html
    
    Change edit account view to now use the simpledialog instead (in account.js)

--- resource/js/account.js
+++ resource/js/account.js
@@ -2,14 +2,30 @@ $(document).ready(function() {
 	$(document).bind("mobileinit", function(){ $.mobile.page.prototype.options.domCache = true; }); //initialize jQuery mobile
 	//get current mobile number from localStorage
 	var mobileNumber = $.Storage.get("mobileNumber");
+	//cache div currentMobileNumber
+	var currentMobileNumber = $("#currentMobileNumber");
 	//and display it
-	$("#currentMobileNumber").html('Current mobile number: ' + mobileNumber);
+	currentMobileNumber.html('Current mobile number: ' + mobileNumber);
 	//when update account button is tapped/clicked on
 	$("#updateAccountButton").live('tap click',function(event,ui) {
-		var newNumber = $("#newMobileNumber").val();
+		var newMobileNumber = $("#newMobileNumber"); //cache the text field
+		var newNumber = newMobileNumber.val();
 		if (newNumber!='') { //if new mobile number entered (i.e new Mobile number field not empty)
 			$.Storage.set('mobileNumber',newNumber); //override the mobile number with the new one in localStorage
-			$.mobile.changePage('accountsaved.html','pop',false,true); //display the pop up dialog upon saved
+			$(this).simpledialog({
+			    'mode' : 'bool',
+			    'prompt' : 'Your account\'s mobile number has been updated',
+			    'buttons' : {
+			      'OK': {
+			        click: function () {
+			        	currentMobileNumber.html("Current mobile number: " + newNumber);
+						newMobileNumber.val(''); //empty the text field
+			        }
+			      },
+			    }
+			  });
+			
+			// $.mobile.changePage('accountsaved.html','pop',false,true); //display the pop up dialog upon saved
 		} else { return false; }
 	});
 });
